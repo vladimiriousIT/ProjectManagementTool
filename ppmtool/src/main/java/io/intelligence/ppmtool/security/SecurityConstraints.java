b472231d2f03b35fd83812f8808eb2f42736ae6a
Vladimir Stratiev <vladistratiev@vladis-mac.local>
2021-04-02 11:01:55 +0300

    Add Heroku config

--- ppmtool/src/main/java/io/intelligence/ppmtool/security/SecurityConstraints.java
+++ ppmtool/src/main/java/io/intelligence/ppmtool/security/SecurityConstraints.java
@@ -6,5 +6,5 @@ public class SecurityConstraints {
   public static final String SECRET_KEY = "SecretKeyToGenerateJWTs";
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
-  public static final long TOKEN_EXPIRATION_TIME = 1000000; //300 Seconds
+  public static final long TOKEN_EXPIRATION_TIME = 3000000;
 }
